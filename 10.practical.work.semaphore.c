12838968c2bc628b380a837dcb5fac633fe6587b
skpxhtnm <35122194+skpxhtnm@users.noreply.github.com>
2018-03-03 11:13:18 +0700

    Practical 10
    Semaphore

---
+++ 10.practical.work.semaphore.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+#define BUFFER_SIZE 10
+
+typedef struct{
+	char type; // 0 = Fried Chicken, 1 = French Fries
+	int amount; // Pieces or weight
+	char unit; // 0 = Pieces , 1 = Grams
+} item;
+
+item buffer[BUFFER_SIZE];
+int first = 0;
+int last = 0;
+
+item newItem(char a,int b, char c){
+	item i;
+	i.type = a;
+	i.amount = b;
+	i.unit = c;
+	return i;
+}
+
+void produce(item *i){
+	while ((first + 1) % BUFFER_SIZE == last){
+	}
+	sem_wait(&sem);
+	memcpy(&buffer[first], i, sizeof(item));
+	first = (first + 1) % BUFFER_SIZE;
+	sem_post(&s);
+}
+
+item *consume(){
+	item *i = malloc(sizeof(item));
+	while (first == last){
+	}
+	sem_wait(&sem);
+	memcpy(i, &buffer[last],sizeof(item));
+	last = (last + 1) % BUFFER_SIZE;
+	return i;
+	sem_post(&s);
+}
+
+void print(item *i){
+	if(i == NULL) return;
+	printf("%s %d %s\n", i->type, i->amount, i->unit);
+}
+
+void *pthread_produce(void *param){
+	item item1, item2, item3;
+	item1 = newItem(0,5,0);
+	item2 = newItem(1,200,1);
+	item3 = newItem(0,10,0);
+	produce(&item1);
+	produce(&item2);
+	produce(&item3);
+}
+
+void *pthread_consume(void *param){
+	print(consume());
+	print(consume());
+}
+
+int sem_init(sem_t *sem, int pshared, unsigned int value);
+int sem_wait(sem_t *sem);
+int sem_post(sem_t *sem);
+int sem_destroy(sem_t *sem);
+
+int main(){
+	sem_init(&sem, 0, 1);
+	pthread_t tid1,tid2 ;
+	pthread_create(&tid1, NULL, pthread_consume, NULL);
+	pthread_create(&tid2, NULL, pthread_consume, NULL);
+	sem_destroy(&sem);
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+}
